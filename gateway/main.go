bbdc2dc7a60bc997bcba3cae2d1627e4ba61476d
浩 <inf2inf2@outlook.com>
2021-12-21 13:55:26 +0800

    allow all request call

--- gateway/main.go
+++ gateway/main.go
@@ -45,14 +45,25 @@ func handle(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 		return
 	}
 
+	accessControlHeaders(w)
 	w.Header().Add("Location", "https://cloudflare-ipfs.com/ipfs/"+avatar)
 	w.WriteHeader(302)
 }
 
 func main() {
 	r := httprouter.New()
+	r.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		accessControlHeaders(w)
+	})
 	r.GET("/v1/:address", handle)
 	r.GET("/v1/:address/:name", handle)
 
 	log.Fatalln(http.ListenAndServe(":8080", r))
+}
+
+func accessControlHeaders(w http.ResponseWriter) {
+	header := w.Header()
+	header.Set("Access-Control-Allow-Methods", "GET")
+	header.Set("Access-Control-Allow-Origin", "*")
+	header.Set("Access-Control-Allow-Headers", "*")
 }
